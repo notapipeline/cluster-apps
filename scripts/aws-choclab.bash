a51a71796c75f4a4e3a688692018faf8236fb864
Martin Proffitt <mproffitt@choclab.net>
2022-09-24 13:20:05 +0200

    Testing crossplane resources
      * Create basic subnet and security group for testing purposes
      * dynamodb
      * VPC imported from existing resource to test this

--- scripts/aws-choclab.bash
+++ scripts/aws-choclab.bash
@@ -6,9 +6,9 @@ aws_access_key_id = $(aws configure get aws_access_key_id --profile $AWS_PROFILE
 aws_secret_access_key = $(aws configure get aws_secret_access_key --profile $AWS_PROFILE)
 EOT
 
-kubectl create secret generic aws-creds \
+kubectl create secret generic choclab-aws-credentials \
     --namespace crossplane-system \
     --dry-run=client \
-    --from-file=creds=./creds.conf -o yaml > namespaces/crossplane-system/secrets/choclab-aws-credentials.yaml
+    --from-file=eu-central-1=./creds.conf -o yaml > namespaces/crossplane-system/secrets/choclab-aws-credentials.yaml
 rm creds.conf
 sops --encrypt --in-place namespaces/crossplane-system/secrets/choclab-aws-credentials.yaml
