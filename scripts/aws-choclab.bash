fae2e897cdab69df441dc42dcaf0ea635a943825
Martin Proffitt <mproffitt@choclab.net>
2022-09-24 13:14:48 +0200

    Add crossplane provider

---
+++ scripts/aws-choclab.bash
@@ -0,0 +1,14 @@
+AWS_PROFILE=default
+
+cat <<EOT > creds.conf
+[default]
+aws_access_key_id = $(aws configure get aws_access_key_id --profile $AWS_PROFILE)
+aws_secret_access_key = $(aws configure get aws_secret_access_key --profile $AWS_PROFILE)
+EOT
+
+kubectl create secret generic aws-creds \
+    --namespace crossplane-system \
+    --dry-run=client \
+    --from-file=creds=./creds.conf -o yaml > namespaces/crossplane-system/secrets/choclab-aws-credentials.yaml
+rm creds.conf
+sops --encrypt --in-place namespaces/crossplane-system/secrets/choclab-aws-credentials.yaml
