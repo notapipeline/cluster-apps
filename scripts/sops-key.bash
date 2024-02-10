a373958b74d7d896b620e8929ebf3214ed0301ac
Martin Proffitt <mproffitt@choclab.net>
2024-02-10 13:55:35 +0100

    bootstrap new clusters

---
+++ scripts/sops-key.bash
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+export KEY_NAME="$1"
+export KEY_COMMENT="$2"
+
+gpg --batch --full-generate-key <<EOF
+%no-protection
+Key-Type: 1
+Key-Length: 4096
+Subkey-Type: 1
+Subkey-Length: 4096
+Expire-Date: 0
+Name-Comment: ${KEY_COMMENT}
+Name-Real: ${KEY_NAME}
+EOF
